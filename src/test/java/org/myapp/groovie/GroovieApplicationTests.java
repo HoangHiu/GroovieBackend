04df508239fd05312409500fa544b213e1263a6e
Hoang Hieu <hoangdinhhieu2105.work@gmail.com>
2024-12-02 21:53:29 +0700

    initial commit

---
+++ src/test/java/org/myapp/groovie/GroovieApplicationTests.java
@@ -0,0 +1,13 @@
+package org.myapp.groovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroovieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
